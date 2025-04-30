8ede85d4204710c1b5b01c4907f312400e38a36a
Karandeep0008 <118888156+Karandeep0008@users.noreply.github.com>
2025-04-30 14:58:57 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/AkshayCV.pdf";
+import pdf from "../../Assets/../Assets/cv karan 243211.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
